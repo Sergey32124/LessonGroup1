952d58e95df09f166699162eb8797835f65b9089
Sergey12432 <tchuxin201@yandex.ru>
2024-07-05 15:59:30 +0300

    Project Lesson Group 1 DZ

--- src/main/java/DZ/group01/ShopTicket/Ticket/TicketServise.java
+++ src/main/java/DZ/group01/ShopTicket/Ticket/TicketServise.java
@@ -1,20 +1,20 @@
 package DZ.group01.ShopTicket.Ticket;
 
 public class TicketServise {
-    public Ticket createTicket(long kilometrs){
+    public Ticket createTicket(long kilometers){
     Ticket ticket=new Ticket();
-    ticket.setDistance_in_km(kilometrs);
-    ticket.setPrice(kilometrs*3);
+    ticket.setDistance_in_km(kilometers);
+    ticket.setPrice(kilometers*3);
     return ticket;
     }
-    public Ticket createTicketWithDiscount(double kilometrs){
+    public Ticket createTicketWithDiscount(double kilometers){
         Ticket ticket= new Ticket();
-        if (kilometrs>=1000){
-            ticket.setPrice(kilometrs*1);
-        }else if(kilometrs>=2000){
-            ticket.setPrice(kilometrs*2);
+        if (kilometers>2000){
+            ticket.setPrice(kilometers*1);
+        }else if(kilometers >= 1000){
+            ticket.setPrice(kilometers*2);
         }else {
-            ticket.setPrice(kilometrs*3);
+            ticket.setPrice(kilometers*3);
         }
         return ticket;
     }
