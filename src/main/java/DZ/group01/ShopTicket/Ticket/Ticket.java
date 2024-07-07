897029acd7f28166e95f493c59c29bf7e48944b4
Sergey12432 <tchuxin201@yandex.ru>
2024-07-07 22:06:58 +0300

    Project Lesson Group 1 DZ Test DZ 1_1 комментарии

--- src/main/java/DZ/group01/ShopTicket/Ticket/Ticket.java
+++ src/main/java/DZ/group01/ShopTicket/Ticket/Ticket.java
@@ -1,5 +1,33 @@
 package DZ.group01.ShopTicket.Ticket;
+/*Создайте класс Ticket
+        Свойства
+        Расстояние в км(тип long)
+        Стоимость
+        Конструктор пустой
+        Гетеры и сеттеры
 
+        Создайте класс TicketServise
+        Метод createTicket
+        Принимает километры
+        И возвращает билет
+        Где стоимость будет расчитана по формуле
+
+        1 км = 3 руб
+
+        Метод createTicketWithDiscount
+        Принимает километры
+        И возвращает билет
+
+        Со стоимостью
+        1 км  = 3 руб
+
+        Но если км будет больше 1000 то
+        1 км = 2 руб
+
+        Если км будет больше 2000
+        1км = 1 руб
+
+        Написать тесты к TicketService.*/
 import lombok.Getter;
 import lombok.Setter;
 import lombok.ToString;
